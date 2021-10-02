be04b06b6efb55cb4c4dbcdec0d098f4ec9ed65e
yonePC <ys.mailaddress0929@gmail.com>
2021-10-03 07:08:30 +0900

    4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str1 = "yone";
+		System.out.println(str1);
+
+		str1 = "DMM WEBCAMP";
+		System.out.println(str1);
+
+		System.out.println(LUCKY_NUMBER);
+	}
+}
