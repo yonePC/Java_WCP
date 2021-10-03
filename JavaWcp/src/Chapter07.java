52ce737bc289ba02d742d0ed5127bd529abcac97
yonePC <ys.mailaddress0929@gmail.com>
2021-10-03 11:54:11 +0900

    8章

--- JavaWcp/src/Chapter07.java
+++ JavaWcp/src/Chapter07.java
@@ -1,6 +1,7 @@
 
 public class Chapter07 {
 	public static void main(String[] args) {
+//		問題1
 		int distance = 12;
 
 		if (distance <= 5) {
@@ -12,28 +13,25 @@ public class Chapter07 {
 		} else {
 			System.out.println("とても遠いです");
 		}
+
 		
-		
-		String color = "red";
-		switch(color) {
+//		問題2
+		String color = "red"; 
+		switch (color) {
 		case "red":
-		System.out.println("赤信号です");
-		break;
-		
+			System.out.println("赤信号です");
+			break;
+
 		case "yellow":
-		System.out.println("黄信号です");
-		break;
-		
+			System.out.println("黄信号です");
+			break;
+
 		case "blue":
-		System.out.println("青信号です");
-		break;
-		
+			System.out.println("青信号です");
+			break;
+
 		default:
-		System.out.println("信号の色ではありません");
+			System.out.println("信号の色ではありません");
 		}
 	}
 }
-
-
-
-
